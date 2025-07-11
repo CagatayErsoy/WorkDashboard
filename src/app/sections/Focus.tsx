3675bf80176e26e94b229fc8223946bed9e749c1
çağatay ersoy <cagatay.ersoy1@gmail.com>
2025-07-10 22:56:08 -0700

    start

---
+++ src/app/sections/Focus.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Focus = () => {
+  return (
+    <div>
+      <h2>Todays Focus</h2>
+    </div>
+  );
+};
+
+export default Focus;
---
+++ src/app/sections/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="w-full flex col align-middle justify-center">
+      <h1>Work Dashboard</h1>
+      {/* Inspriational quote  */}
+      <p> work hard play hard</p>
+    </div>
+  );
+};
+
+export default Header;
